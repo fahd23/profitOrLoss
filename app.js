e80ef7bb9156782e806d1f22abdd0340c8e61b59
fahd23 <fahdpno@gmail.com>
2021-09-17 18:12:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,24 +10,28 @@ function calculateProfitAndLoss(){
     const costPrice = Number(initialPrice.value)
     const noOfQty = Number(quantityOfStocks.value);
     const sellingPrice = Number(currentPrice.value);
-
-    if (costPrice > sellingPrice) {
+    if(costPrice > 0 && noOfQty > 0 && sellingPrice >= 0){
+      if (costPrice > sellingPrice) {
         const loss = (costPrice - sellingPrice) * noOfQty;
         const lossPercentage = ((costPrice - sellingPrice) / costPrice) * 100;
-        showOutput(`Hey, the loss is ${loss.toFixed(2)} and the percent is ${lossPercentage.toFixed(2)}% 😥`);
+        showOutput(`Oh oh!, the loss is ${loss.toFixed(2)} and the  loss percent is ${lossPercentage.toFixed(2)}% 😥`);
       } 
       else if (sellingPrice > costPrice) {
         const profit = (sellingPrice - costPrice) * noOfQty;
         const profitPercentage = ((sellingPrice - costPrice) / costPrice) * 100;
         if (profitPercentage > 50){
-            showOutput(`Hey, the profit is ${profit.toFixed(2)} and the percent is ${profitPercentage.toFixed(2)}% 🤑`);
+            showOutput(`Hey, the profit is ${profit.toFixed(2)} and the Profit percent is ${profitPercentage.toFixed(2)}% 🤑`);
         }else{
-            showOutput(`Hey, the profit is ${profit.toFixed(2)} and the percent is ${profitPercentage.toFixed(2)}% 💰`);        }
+            showOutput(`Hey, the profit is ${profit.toFixed(2)} and the Profit percent is ${profitPercentage.toFixed(2)}% 💰`);        }
         
       }
       else {
-        showOutput(`No pain no gain and no gain no pain 😶`);
+        showOutput("No pain no gain and no gain no pain 😶");
       }
+    }else{
+      showOutput("Please enter the valid inputs (Greater than 0)");
+    }
+
 
 }
 
